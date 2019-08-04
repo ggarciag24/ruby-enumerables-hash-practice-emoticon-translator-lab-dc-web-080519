e3fd83df58ac808e5e8ca0e20095c842373de4a7
German Garcia <ggarciag.business@outlook.com>
2019-08-04 23:00:24 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,11 +1,58 @@
-# require modules here
+require "yaml"
+require "pry"
 
-def load_library
-  # code goes here
+def load_library(emoticon)
+loadout = YAML.load_file('./lib/emoticons.yml')
+  output_hash = {
+    :get_meaning => {},
+    :get_emoticon => {}
+  }
+  loadout.each do |word_meaning, emojis|
+    binding.pry
+    
+  end
+
+
+
+  # output_hash = {
+  #   :get_meaning => {
+  #     # jap_emoji: word_meaning
+  #     "☜(⌒▽⌒)☞" => "angel",
+  #     "ヽ(ｏ`皿′ｏ)ﾉ" => "angry",
+  #     "(ΘεΘ;)" => "bored",
+  #     "(゜.゜)" => "confused",
+  #     "(#^.^#)" => "embarrassed",
+  #     ">゜))))彡" => "fish",
+  #     "(^0_0^)" => "glasses",
+  #     "(￣ー￣)" => "grinning",
+  #     "（＾ｖ＾）" => "happy",
+  #     "*^3^)/~☆" => "kiss",
+  #     "(Ｔ ▽ Ｔ)" => "sad",
+  #     "o_O" => "surprised",
+  #     "(^_-)" => "wink"
+  #   },
+  #   :get_emoticon => {
+  #     # eng_emoji: jap_emoji
+  #     "O:)" => "☜(⌒▽⌒)☞",
+  #     ">:(" => "ヽ(ｏ`皿′ｏ)ﾉ",
+  #     ":O" => "(ΘεΘ;)",
+  #     "%)" => "(゜.゜)",
+  #     ":$" => "(#^.^#)",
+  #     "><>" => ">゜))))彡",
+  #     "8D" => "(^0_0^)",
+  #     "=D" => "（￣ー￣）",
+  #     ":)" => "（＾ｖ＾）",
+  #     ":*" => "*^3^)/~☆",
+  #     ":'(" => "(Ｔ▽Ｔ)",
+  #     ":o" => "o_O",
+  #     ";)" => "(^_-)"
+  #   }
+  # }
+
 end
 
-def get_japanese_emoticon
-  # code goes here
+def get_japanese_emoticon(file_path, emoticon)
+  load_library(file_path)
 end
 
 def get_english_meaning
